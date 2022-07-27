98a5780d44fc1dfc2161d5712304f76b20d8edb3
Josh Ahles <josh@showit.co>
2022-07-27 08:13:14 -0700

    added favicon and share image

--- marvelProject/urls.py
+++ marvelProject/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('search-results', views.searchresults, name='search-results'),
+    path('info', views.info, name='info')
 ]
--- marvelProject/views.py
+++ marvelProject/views.py
@@ -1,6 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     context = {}
     return render(request, 'marvelProject/index.html', context)
+
+def searchresults(request):
+    context = {}
+    return render(request, 'marvelProject/search-results.html', context)
+    
+def info(request):
+    context = {}
+    return render(request, 'marvelProject/info.html', context)
